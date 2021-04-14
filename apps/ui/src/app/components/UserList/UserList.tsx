91232a49561da8ddb872597a013e2df3886dc385
Geoff Whitehead <geoff1012@gmail.com>
2021-04-14 23:30:48 +0100

    update users list to render table

--- apps/ui/src/app/components/UserList/UserList.tsx
+++ apps/ui/src/app/components/UserList/UserList.tsx
@@ -1,21 +1,101 @@
 import React from 'react';
+import { useTable } from 'react-table';
+import styled from 'styled-components';
 
 type UserList = {
   users: any[]; // TODO - pull from lib
 };
 
 export const UserList: React.FC<UserList> = ({ users }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'First Name',
+        accessor: 'firstName',
+      },
+      {
+        Header: 'Last Name',
+        accessor: 'lastName',
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone',
+      },
+      {
+        Header: 'Actions',
+        Cell: () => {
+          return <button>View photo</button>;
+        },
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data: users });
+
   return (
-    <div>
-      {users.length === 0 && <p>No users found...</p>}
-      {users.map((user) => {
-        return (
-          <div key={user.id}>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-          </div>
-        );
-      })}
-    </div>
+    <StyledTable {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <StyledCell {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </StyledCell>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <StyledRow {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <StyledCell {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </StyledCell>
+                );
+              })}
+            </StyledRow>
+          );
+        })}
+      </tbody>
+    </StyledTable>
   );
 };
+
+const StyledTable = styled.table`
+  border-spacing: 0;
+  border: 1px solid black;
+`;
+
+const StyledCell = styled.td`
+  margin: 0;
+  padding: 0.5rem;
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+
+  :last-child {
+    border-right: 0;
+  }
+`;
+
+const StyledRow = styled.tr`
+  :last-child {
+    td {
+      border-bottom: 0;
+    }
+  }
+`;
