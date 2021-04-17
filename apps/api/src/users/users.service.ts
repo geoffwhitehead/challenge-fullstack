4ecc95e5fc34cd43b475b8997cce4f7be3266b5f
Geoff Whitehead <geoff1012@gmail.com>
2021-04-17 19:14:14 +0100

    remove file from temp uploads dir

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User, UserDto } from '@org/types';
+import * as fs from 'fs';
 import { Repository } from 'typeorm';
 import { AssetsService } from '../assets/assets.service';
 import { UserEntity } from './user.entity';
@@ -31,10 +32,19 @@ export class UsersService {
       cannedAcl: 'public-read',
       bucket: this.configService.get('s3Bucket'),
     });
+
     const newUser = await this.usersRepository.create({
       ...user,
       photo: asset.id,
     });
+
+    /**
+     * Remove the uploaded file
+     * Note: Another option here would be to use multer-s3 in the controller
+     * to upload the file directly to aws, skipping this step entirely.
+     */
+
+    await fs.unlinkSync(photo.path);
     return this.usersRepository.save(newUser);
   }
 }
