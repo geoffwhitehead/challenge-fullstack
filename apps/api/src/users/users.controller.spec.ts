58bbe86b1ffd15a8d3eff3d13cc9e1c9fc5db3b0
Geoff Whitehead <geoff1012@gmail.com>
2021-04-18 11:01:07 +0100

    add users module tests

--- apps/api/src/users/users.controller.spec.ts
+++ apps/api/src/users/users.controller.spec.ts
@@ -1,23 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+const testUsers = [
+  {
+    id: 123,
+    firstName: 'Peter',
+    lastName: 'Griffin',
+    email: 'petergriffin@familyguy.com',
+    phone: '123123123',
+    photo: 456,
+  },
+];
+
+const testFile = {
+  fieldname: 'qwe',
+  originalname: 'qwe',
+  encoding: 'qwe',
+  mimetype: 'qwe',
+  destination: 'qwe',
+  filename: 'qwe',
+  path: 'qwe',
+  size: 123,
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            getUsers: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve(testUsers)),
+            createUser: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve(testUsers[0])),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined();
-  // });
-
   it('should be defined', () => {
-    return true;
-    // expect(service).toBeDefined();
+    expect(controller).toBeDefined();
+  });
+
+  describe('getUsers', () => {
+    it('returns an array of users', () => {
+      expect(controller.getUsers()).resolves.toEqual(testUsers);
+      expect(service.getUsers).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getUsers', () => {
+    it('creates a user', () => {
+      expect(controller.createUser(testFile, testUsers[0])).resolves.toEqual(
+        testUsers[0]
+      );
+      expect(service.createUser).toHaveBeenCalledWith({
+        photo: testFile,
+        user: testUsers[0],
+      });
+    });
   });
 });
--- apps/api/src/users/users.service.spec.ts
+++ apps/api/src/users/users.service.spec.ts
@@ -1,22 +1,118 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ConfigService } from 'aws-sdk';
+import { Repository } from 'typeorm';
+import { AssetEntity } from '../assets/asset.entity';
+import { AssetsService } from '../assets/assets.service';
+import { UserEntity } from './user.entity';
+import { UploadedFile } from './users.controller';
 import { UsersService } from './users.service';
+
+const testUsers = [
+  {
+    id: 123,
+    firstName: 'Peter',
+    lastName: 'Griffin',
+    email: 'petergriffin@familyguy.com',
+    phone: '123123123',
+    photo: 456,
+  },
+];
+
+const testFile: UploadedFile = {
+  fieldname: 'qwe',
+  originalname: 'qwe',
+  encoding: 'qwe',
+  mimetype: 'qwe',
+  destination: 'qwe',
+  filename: 'qwe',
+  path: 'qwe',
+  size: 123,
+};
+
+const testAsset: AssetEntity = {
+  id: 123,
+  mimetype: 'image/png',
+  size: 1000,
+  bucket: 'my-aws-bucket',
+  key: 'folder/file.ext',
+  location: 'https://my-bucket.s3.amazonaws.com/my-key',
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let assetsService: AssetsService;
+  let configService: ConfigService;
+  let repo: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue(testUsers),
+            create: jest.fn().mockResolvedValue(testUsers[0]),
+            save: jest.fn().mockResolvedValue(testUsers[0]),
+          },
+        },
+        {
+          provide: AssetsService,
+          useValue: {
+            createAsset: jest.fn().mockResolvedValue(testAsset),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            s3Bucket: 'my-aws-bucket',
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    assetsService = module.get<AssetsService>(AssetsService);
+    configService = module.get<ConfigService>(ConfigService);
+    repo = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
   });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
   it('should be defined', () => {
-    return true;
-    // expect(service).toBeDefined();
+    expect(service).toBeDefined();
+    expect(assetsService).toBeDefined();
+    expect(configService).toBeDefined();
+    expect(repo).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should return a created user', async () => {
+      expect(
+        service.createUser({
+          user: {
+            firstName: 'Peter',
+            lastName: 'Griffin',
+            email: 'petergriffin@familyguy.com',
+            phone: '123123123',
+          },
+          photo: testFile,
+        })
+      ).resolves.toEqual(testUsers[0]);
+
+      expect(assetsService.createAsset).toHaveBeenCalledWith({
+        asset: testFile,
+        cannedAcl: 'public-read',
+        bucket: 'my-aws-bucket',
+      });
+      expect(repo.create).toBeCalledTimes(1);
+      expect(repo.create).toHaveBeenCalledWith({
+        ...testUsers[0],
+        photo: testAsset.id,
+      });
+      expect(repo.save).toHaveBeenCalledWith({
+        ...testUsers[0],
+        photo: testAsset.id,
+      });
+    });
   });
 });
